f2f13d9dddcac9a8395c25bb09f23f02b153c798
Arsalan Khan <nalasrakhan@gmail.com>
2023-01-30 17:42:14 +0400

    Fixing Visa 62/63

--- src/main/java/org/hablo/mastercard/iso/MCDMSParser.java
+++ src/main/java/org/hablo/mastercard/iso/MCDMSParser.java
@@ -1,24 +1,59 @@
 package org.hablo.mastercard.iso;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hablo.ISOMsgParserSupport;
+import org.hablo.mastercard.util.DE110Parser;
+import org.hablo.mastercard.util.DE48Parser;
+import org.hablo.mastercard.util.DE61Parser;
+import org.hablo.mastercard.util.DEParserSupport;
+import org.jpos.ee.BLException;
 import org.jpos.iso.ISOException;
 import org.jpos.iso.ISOMsg;
 import org.jpos.iso.packager.GenericPackager;
 
 public class MCDMSParser implements ISOMsgParserSupport {
-    static GenericPackager packager;
 
-    public ISOMsg parse(byte[] d){
+    static GenericPackager packager;
+    static List<Class> mcDEParsers = new ArrayList<>();
+
+    static {
+        mcDEParsers.add(DE61Parser.class);
+        mcDEParsers.add(DE48Parser.class);
+        mcDEParsers.add(DE110Parser.class);
+    }
+
+    public ISOMsg parse(byte[] d) {
         ISOMsg m = null;
         try {
             packager = new GenericPackager("jar:packager/mc_dms.xml");
             m = packager.createISOMsg();
             m.setPackager(packager);
             m.unpack(d);
-        } catch (ISOException e) {
+            m.dump(System.out, "");
+            if (mcDEParsers != null) {
+                System.out.println("----- Parsers ------");
+                expandDE(m, mcDEParsers);
+            }
+        } catch (ISOException | BLException e) {
             System.out.println("Exception occurred...");
             e.printStackTrace();
         }
         return m;
+    }
+
+
+    public <T> void expandDE(ISOMsg m, List<Class> dEParsers) throws BLException, ISOException {
+        for (Class<T> c : dEParsers) {
+            try {
+                T o = c.getDeclaredConstructor(new Class[]{ISOMsg.class}).newInstance(m);
+                if (o instanceof DEParserSupport) {
+                    ((DEParserSupport) o).dump(System.out, "");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/org/hablo/visa/iso/VisaBaseIParser.java
+++ src/main/java/org/hablo/visa/iso/VisaBaseIParser.java
@@ -4,6 +4,7 @@ import org.hablo.ISOMsgParserSupport;
 import org.jpos.iso.ISOException;
 import org.jpos.iso.ISOMsg;
 import org.jpos.iso.packager.GenericPackager;
+import org.jpos.util.Logger;
 
 public class VisaBaseIParser implements ISOMsgParserSupport {
     static GenericPackager packager;
@@ -12,9 +13,11 @@ public class VisaBaseIParser implements ISOMsgParserSupport {
         ISOMsg m = null;
         try {
             packager = new GenericPackager("jar:packager/base1.xml");
+            packager.setLogger(Logger.getLogger("Q2"), getClass().getName());
             m = packager.createISOMsg();
             m.setPackager(packager);
             m.unpack(d);
+            m.dump(System.out, "");
         } catch (ISOException e) {
             System.out.println("Exception occurred...");
             e.printStackTrace();
